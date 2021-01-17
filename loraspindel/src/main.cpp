b1539e25e6d0f2a1c66af6f59a60230d754b50c9
tim <tim.hallinan@yahoo.co.uk>
2021-01-18 12:21:13 +1300

    Tidy up

--- loraspindel/src/main.cpp
+++ loraspindel/src/main.cpp
@@ -3,8 +3,7 @@
 
 ///PINS used
 //LORA RA01 module SPI 11 12(MISO)  13      DIO0 2,   reset 9,  NSS 10,
-//cap sensors 17 14 15 16
-// not connected 0(rx) 1(Tx)   3 4 5 6 7 8    A4 A5 A6 A7 
+// not connected 0(rx) 1(Tx)   3 4 5 6 7 8    A4 A5 A6 A7 cap sensors 17 14 15 16
 
 //#define DEBUG   //If you comment this line, the DPRINT & DPRINTLN lines are defined as blank.
 //test
@@ -29,14 +28,11 @@ int readVcc(void);
 double GetTemp(void);
 byte batteryVoltageCompress (int batvoltage);
 byte temperatureCompress (double temperature);
-long GetCapacitance(int repeats, int OUT_PIN, int IN_PIN);
-void waitForAck();
 void UnusedPinsPullup();
 
 //varables
-const byte nodeID=1;//must be unique for each device
-boolean ackReceived =0;
-const int sleepDivSixteen =54; //sleep time divided by 16 (seconds)  36=10minutes, 54=15minutes
+const byte nodeID=2;//must be unique for each device
+const int sleepDivSixteen =1; //sleep time divided by 16 (seconds)  36=10minutes, 54=15minutes
 RH_RF95 rf95(10, 2); // Select, interupt. Singleton instance of the radio driver
 static const RH_RF95::ModemConfig radiosetting = {
     BW_SETTING<<4 | CR_SETTING<<1 | ImplicitHeaderMode_SETTING,
@@ -86,12 +82,11 @@ UnusedPinsPullup();//set unused pined to INPUTPULLUP to save power
     rf95.init();//try again
   }
   // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
-
   // The default transmitter power is 13dBm, using PA_BOOST.
   // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
   // you can set transmitter powers from 5 to 23 dBm:
 
-rf95.setTxPower(20, false);//was 17
+rf95.setTxPower(5, false);//2-20dBm
 DPRINTln("init ok");
 rf95.setModemConfig(RH_RF95::Bw125Cr48Sf4096);// BW =125kHz, CRC4/8, sf 4096
 delay(10);
@@ -112,85 +107,26 @@ Serial.print("ADCSRA=");Serial.println(ADCSRA,BIN);
 //------------------------------------------------------------------------------
 void loop()
 {
-  
   //put radio and Atmega to sleep
   rf95.sleep();//FIFO data buffer is cleared when the device is put in SLEEP mode
   //therefore any acks destined for other devices should be cleared
   delay(10);
-
-
-//pinMode (9, INPUT);// if the reset pin is held high on the RA-01 it will somtimes draw hundreds of micro amps
-//pinMode(12, INPUT_PULLUP);// mISO
-//digitalWrite(10, LOW);//Spi CHIP SELECT
   DPRINTln(" sleep");delay(10);
   for (int i=0; i < sleepDivSixteen; i++){
   LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_ON);//sleep atmega
   LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_ON);
   }
-
+//pinMode (9, INPUT);// if the reset pin is held high on the RA-01 it will somtimes draw hundreds of micro amps
+//pinMode(12, INPUT_PULLUP);// mISO
+//digitalWrite(10, LOW);//Spi CHIP SELECT
   //digitalWrite(10, HIGH);//Spi CHIP SELECT
-  delay(50);DPRINTln("waking");
-  
+  delay(50);
+  DPRINTln("waking");
   DPRINT(millis());
-  long capTotal;
-
-//capsensor 1
-GetCapacitance(1, 17, 14);//prime
-capTotal=GetCapacitance(6, 17, 14);
-capTotal+=GetCapacitance(6, 17, 14);
-//capTotal+=GetCapacitance(6, 17, 14);
-capTotal=capTotal/2;//get mean
-DPRINT(" mainloop freeMemory()=");
-DPRINTln(freeMemory());
+  DPRINT(" mainloop freeMemory()=");
+  DPRINTln(freeMemory());
   DPRINT("  ");DPRINT(millis());
 
-  DPRINT(" capacitive sensor 1");DPRINTln(capTotal);
-  DPRINT(" capLobyte ");DPRINT( (byte)(capTotal%256));
-  DPRINT(" caphibyte "); DPRINT ((byte)(capTotal>>8));
-
-//start building txpayload
-  if ((capTotal >=0) & (capTotal <= 65535)){
-  txpayload.capsensor1Lowbyte=(byte)(capTotal%256);
-  txpayload.capsensor1Highbyte=(byte)(capTotal>>8);
-  }
-  else {txpayload.capsensor1Lowbyte=0;txpayload.capsensor1Highbyte=0;}
-  delay(50);
-//next sensor
-  GetCapacitance(1, 17, 15);//prime
-  capTotal=GetCapacitance(6, 17, 15);
-  capTotal+=GetCapacitance(6, 17, 15);
-  //capTotal+=GetCapacitance(6, 17, 15);
-  capTotal=capTotal/2;//get mean
-
-
-  //start building txpayload
-    if ((capTotal >=0) & (capTotal <= 65535)){
-    txpayload.capsensor2Lowbyte=(byte)(capTotal%256);
-    txpayload.capsensor2Highbyte=(byte)(capTotal>>8);
-    }
-    else {txpayload.capsensor2Lowbyte=0;txpayload.capsensor2Highbyte=0;}
-
-delay(50);
-//next sensor
-  GetCapacitance(1, 17, 16);//prime
-  capTotal=GetCapacitance(6, 17, 16);
-  capTotal+=GetCapacitance(6, 17, 16);
-  //capTotal+=GetCapacitance(6, 17, 16);
-  capTotal=capTotal/2;//get mean
-
-
-  //start building txpayload
-    if ((capTotal >=0) & (capTotal <= 65535)){
-    txpayload.capsensor3Lowbyte=(byte)(capTotal%256);
-    txpayload.capsensor3Highbyte=(byte)(capTotal>>8);
-    }
-    else {txpayload.capsensor3Lowbyte=0;txpayload.capsensor3Highbyte=0;}
-
-  delay(10);
-  if (ackReceived==true) {RSSI=abs(rf95.lastRssi());}
-   else{RSSI=0;}
-  txpayload.rssi = RSSI;
-  delay(10);
   int battery= readVcc();
   DPRINT("batvoltage=");DPRINT(battery);
   txpayload.voltage=batteryVoltageCompress(battery);
@@ -201,17 +137,12 @@ delay(50);
 
 //send packet
   rf95.send((uint8_t *)tx_buf, zize);
-  ackReceived =0;// clear previous ack.  Not used unless waitForAck() is called
   delay(500);
   rf95.waitPacketSent(5000);//timeout after 5 seconds
   DPRINTln("packet sent");
-  //waitForAck();// not used to save battery.  Can be enabled if wanted
   delay(20);
-
   
 }
-
-
 
 
 double GetTemp(void)
@@ -219,11 +150,9 @@ double GetTemp(void)
   unsigned int wADC;
   double t;
 
-  // The internal temperature has to be used
-  // with the internal reference of 1.1V.
-  // Channel 8 can not be selected with
-  // the analogRead function yet.
-
+  // The internal temperature has to be used// with the internal reference of 1.1V.
+    // Channel 8 can not be selected with// the analogRead function yet.
+  
   // Set the internal reference and mux.
   ADMUX = (_BV(REFS1) | _BV(REFS0) | _BV(MUX3));
   ADCSRA |= _BV(ADEN);  // enable the ADC
@@ -281,92 +210,6 @@ int readVcc(void) // Returns actual value of Vcc (x 1000)
 
    }
 
-   long GetCapacitance(int repeats, int OUT_PIN, int IN_PIN)
-   {
-  //clock_prescale_set(clock_div_1);
-     //const int OUT_PIN = A4;//A8=4, A7=6
-     //const int IN_PIN = A5;
-    RunningMedian samples = RunningMedian(repeats);
-     const float IN_STRAY_CAP_TO_GND = 24.48;
-     const float IN_CAP_TO_GND  = IN_STRAY_CAP_TO_GND;
-     const int MAX_ADC_VALUE = 1023;
-
-   float capacitance;
-
-   pinMode(OUT_PIN, OUTPUT);
-   pinMode(IN_PIN, OUTPUT);
-
-   for (int i=0; i < repeats; i++){
-
-   pinMode(IN_PIN, INPUT);
-   //delay(1);
-   digitalWrite(OUT_PIN, HIGH);
-   int val = analogRead(IN_PIN);
-   digitalWrite(OUT_PIN, LOW);
-   samples.add(val);
-   if (val < 1023)//was 1000
-   {
-     pinMode(IN_PIN, OUTPUT);
-
-     capacitance = (float)val * IN_CAP_TO_GND / (float)(MAX_ADC_VALUE - val);
-
-     DPRINT(F("Cap Value = "));
-     DPRINT(capacitance, 3);
-     DPRINT(F(" pF ("));
-     DPRINT(val);
-     DPRINTln(F(") "));
-
-
-   }
-
-   //while (millis() % 1000 != 0);
-   delay(100);
-   }
-   //DPRINT(" freeMemory()=");
-   //DPRINTln(freeMemory());
-     float temp = samples.getMedian();
-     capacitance = 10*(float)temp * IN_CAP_TO_GND / (float)(MAX_ADC_VALUE - temp);
-     return (long)capacitance;
-   }
-
-
-void waitForAck(){
-// Now wait for a reply
-  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-  uint8_t len = sizeof(buf);
-
-  DPRINT("waiting to rx, millis = ");DPRINT(millis());
-  unsigned long temptimer = millis();
-
-  //DPRINT("available "); DPRINTln(rf95.available());
-  if (rf95.waitAvailableTimeout(10000))//was10000
-  {
-    // Should be a reply message for us now
-    if (rf95.recv(buf, &len))
-   {
-      DPRINT(" got reply , time for ack=");DPRINT(millis()-temptimer); DPRINT(" millis "); DPRINT(millis());
-      //rf95.printBuffer("Got:", buf, len);
-      DPRINT(" local com voltage = ");DPRINT(txpayload.voltage);
-      DPRINT("    local RSSI = ");DPRINT(rf95.lastRssi(), DEC);
-      DPRINT("    local snr = ");DPRINT(rf95.lastSNR(), DEC);
-      DPRINT(" rx=");DPRINT(buf[0], DEC);DPRINT(" nodeid=");DPRINT(buf[1], DEC);
-      //check message is an ack for this node
-      if ((buf[0]==170) and (buf[1]==nodeID)) {
-        ackReceived=true;
-        DPRINTln(" ackReceived=true");
-      }
-
-    }
-    else
-    {
-      DPRINTln("recv failed");
-    }
-  }
-  else
-  {
-    DPRINTln("No reply");}
-rf95.recv(buf, &len);// clear buffer just in case}
-}
 
 void UnusedPinsPullup(){
 
@@ -393,19 +236,11 @@ pinMode(A4, OUTPUT); digitalWrite(A4, LOW);
 pinMode(A5, OUTPUT); digitalWrite(A5, LOW);
 pinMode(A6, OUTPUT); digitalWrite(A6, LOW);
 pinMode(A7, OUTPUT); digitalWrite(A7, LOW);
+//cap sensors were 17 14 15 16
+pinMode(17, OUTPUT); digitalWrite(17, LOW);
+pinMode(14, OUTPUT); digitalWrite(14, LOW);
+pinMode(15, OUTPUT); digitalWrite(15, LOW);
+pinMode(16, OUTPUT); digitalWrite(16, LOW);
 
-/*
 
-  for (int i = 3; i <= 8; i++)
-    {
-    pinMode (i, INPUT_PULLUP);    // to save power
-    //digitalWrite (i, LOW);  //     ditto
-    }
-    
-
-  for (int i = 18; i <= 21; i++)
-    {
-    pinMode (i, INPUT_PULLUP);    // to save power
-    //digitalWrite (i, LOW);  //     ditto
-    }*/
 }
